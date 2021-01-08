84d2e2e7459d4d0683f1cf1521b3920ebb5efcc6
jiao <jiaoyafei@cmcm.com>
2021-01-08 09:46:18 +0800

    fix: update ci

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,16 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
 npm run build
-
-# 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'b.xugaoyi.com' > CNAME
+echo 'myhost.com' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:joyous-x/blog.git
 else
-  msg='来自github actions的自动部署'
+  msg='ci from github actions'
   githubUrl=https://joyous-x:${GITHUB_TOKEN}@github.com/joyous-x/blog.git
   git config --global user.name "jiao"
   git config --global user.email "929843628@qq.com"
@@ -25,14 +22,15 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
+: <<'COMMENT'
 # deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+echo 'myhost_a.com\myhost_b.com' > CNAME  # 自定义域名
 echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:xgy/xgy.git
+  codingUrl=git@e.coding.net:blog/blog.git
 else
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/blog/blog.git
 fi
 git add -A
 git commit -m "${msg}"
@@ -40,3 +38,4 @@ git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
+COMMENT
